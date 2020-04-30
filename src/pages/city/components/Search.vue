53e0eeaab3c9951a923d05e8e2b3edf90a8523f4
Wind-tsai <33213953+Wind-tsai@users.noreply.github.com>
2020-04-30 09:49:37 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,64 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <!-- 让下面这句话在有搜索结果时不显示，使用v-show -->
+        <li class="search-item border-bottom" v-show="hadNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  import Bscroll from 'better-scroll'
   export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data () {
+      return {
+        keyword:'',
+        list:[],
+        timer:null
+      }
+    },
+    computed:{
+      hadNoData () {
+        return !this.list.length
+      }
+    },
+    watch:{
+      keyword () {
+        if(this.timer) {
+          clearTimeout(this.timer)
+        }
+        //添加删除搜索内容后结果清空的效果
+        if(!this.keyword) {
+          this.list = []
+          return
+        }
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities){
+            this.cities[i].forEach((value)=> {
+              if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) >-1){
+                result.push(value)
+              }
+            })
+          }
+          this.list = result
+        },100)
+      }
+    },
+    // 添加滚动效果
+    mounted () {
+      this.scroll = new Bscroll(this.$refs.search)
+    }
   }
 </script>
 
@@ -25,4 +77,19 @@
     border-radius .06rem
     color #666
     padding 0 .1rem
+  .search-content
+   z-index 1
+   overflow hidden
+   position absolute
+   top 1.58rem
+   left 0
+   right 0
+   bottom 0
+   background #eee
+   .search-item
+    line-height .62rem
+    padding-left .2rem
+    color #666666
+    background #fff
+
 </style>
